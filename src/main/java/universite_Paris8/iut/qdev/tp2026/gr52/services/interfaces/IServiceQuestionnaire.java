bd13491bf5d0ebc9d1795b311f9bd106b927c3d1
abdellah <abelaiboud@iut.univ-paris8.fr>
2026-03-20 12:08:47 +0100

    amélioration interface des services

--- src/main/java/universite_Paris8/iut/qdev/tp2026/gr52/services/interfaces/IServiceQuestionnaire.java
+++ src/main/java/universite_Paris8/iut/qdev/tp2026/gr52/services/interfaces/IServiceQuestionnaire.java
@@ -1,11 +1,12 @@
 package universite_Paris8.iut.qdev.tp2026.gr52.services.interfaces;
 
+import java.io.File;
 import java.util.List;
 import universite_Paris8.iut.qdev.tp2026.gr52.commons.dtos.QuestionnaireDTO;
 
 public interface IServiceQuestionnaire {
 
-    List<QuestionnaireDTO> chargerFichier(String cheminFichier);
+    public File chargerFichier(String cheminFichier);
 
-    List<QuestionnaireDTO> fournirListeQuestionnaire();
+    public List<QuestionnaireDTO> fournirListeQuestionnaire(File csvFile);
 }
